cca7ba86eacd56e75ce3e34bc45af467a5fe1e45
Cody Green <cody@codygreen.com>
2023-12-29 08:36:25 -0600

    add N4A

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type = string
+  description = "value of the resource group name"
+}
+variable "location" {
+  type = string
+  description = "value of the location"
+  default = "westus2"
+}
